b22939dc2d162f6a63cf8cfc5f9d386f8b64cf1c
Thomas Minshull <thomasminshull@gmail.com>
2018-03-27 17:17:06 -0700

    Fix bug with NetworkManager.exchangeRateforCurrency method passing invalid url to downloadTask

--- dailyExchange/dailyExchange/Models/NetworkingManager.swift
+++ dailyExchange/dailyExchange/Models/NetworkingManager.swift
@@ -51,26 +51,19 @@ struct ECBURLGenerator {
 
 class NetworkManager {
     func exchangeRateforCurrency(_ currency: Currency, with base: Currency, completion: @escaping (String) -> ()) {
-//        let urlString = "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.USD.EUR.SP00.A?lastNObservations=1"
-         let urlString = ECBURLGenerator.ecburlCompairing(last: 1, of: currency, to: base, over: .daily)
-        let url = URL(fileURLWithPath: urlString)
-        
-        // https://sdw-wsrest.ecb.europa.eu/service/data/EXR/M.USD.EUR.SP00.A
-        let downloadTask = URLSession.shared.downloadTask(with: url) { (data, response, error) in
-            /*
-             NOTE: the following error will be passed if the user requests two currencies for which there is not an exchange rate for. (unfortuantly, not all exchange rates are represented in the api
-             
-             Error Domain=NSURLErrorDomain Code=-1100 "The requested URL was not found on this server." UserInfo={NSUnderlyingError=0x6000004484c0 {Error Domain=kCFErrorDomainCFNetwork Code=-1100 "(null)"}, NSErrorFailingURLStringKey=file:///https:/sdw-wsrest.ecb.europa.eu/service/data/EXR/D.AED.ANG.SP00.A%3FlastNObservations=1, NSErrorFailingURLKey=file:///https:/sdw-wsrest.ecb.europa.eu/service/data/EXR/D.AED.ANG.SP00.A%3FlastNObservations=1, NSLocalizedDescription=The requested URL was not found on this server.
-            */
+        let urlString = ECBURLGenerator.ecburlCompairing(last: 1, of: currency, to: base, over: .daily)
+        let url = URL(string: urlString)!
+        let downloadTask = URLSession.shared.downloadTask(with: url) { (path, response, error) in
             
-            print("data: \(data); response: \(response); error: \(error)")
-            
+            print("data: \(path); response: \(response); error: \(error)")
             
             completion("temp string")
         }
         
         downloadTask.resume()
     }
+    
+    
  
 }
 
